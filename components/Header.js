6f1ffdc3e3a557fcc275fc9d3aec084b2a09a054
kajava-juri <jyri.kajava@tptlive.ee>
2022-11-24 11:32:19 +0200

    added layout and header components, created the hotkeys list page

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+export default function Header(){
+    return(
+        <ul className="nav justify-content-between">
+
+            <li className="nav-item">
+                <a className="nav-link active" href="#">
+                    <button>About us</button>
+                </a>
+            </li>
+
+            <li className="nav-item">
+                <a className="nav-link mainHeader" href="#">PHOTOSHOP HOTKEYS</a>
+            </li>
+
+            <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,142 @@
-import Head from 'next/head'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <div className="container">
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <p style={{fontSize: 36, fontWeight: 400, fontFamily: "Inter", color: "#252DFB;"}}>Aliquip irure elit officia aliqua magna occaecat sint minim cillum ea officia ex laborum eu. Cillum in proident esse quis aliqua irure commodo duis non enim. Officia consectetur aliqua veniam duis esse tempor magna sunt et cillum quis eiusmod laboris. Tempor velit aliquip ut sint cillum elit amet tempor et esse duis.</p>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      </div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <div className="container-fluid mx-5">
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      <div className="row mt-3 g-4">
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <div className="col-md-4">
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+              <div>
+
+                  <h4 className="hkTitle">Change Image Size</h4>
+
+                  <p className="hkDescription">Control + Alt + i (Command + Option + i )</p>
+
+              </div>
+
+          </div>
+
+          <div className="col-md-4">
+
+              <div>
+
+                  <h4 className="hkTitle">Change canvas Size</h4>
+
+                  <p className="hkDescription">Control + Alt + c (Command + Option + c )</p>
+
+              </div>
+
+          </div>
+
+          <div className="col-md-4">
+
+              <div>
+
+                  <h4 className="hkTitle">Zoom in</h4>
+
+                  <p className="hkDescription">Control + + (Command + +)</p>
+
+              </div>
+
+          </div>
+
+          <div className="col-md-4">
+
+              <div>
+
+                  <h4 className="hkTitle">Zoom Out</h4>
+
+                  <p className="hkDescription">Control + - (Command + - )</p>
+
+              </div>
+
+          </div>
+
+          <div className="col-md-4">
+
+              <div>
+
+                  <h4 className="hkTitle">Scale Proportionately</h4>
+
+                  <p className="hkDescription">Hold the shift key while selecting the object</p>
+
+              </div>
+
+          </div>
+
+          <div className="col-md-4">
+
+              <div>
+
+                  <h4 className="hkTitle">Scale in place (from center of the object)</h4>
+
+                  <p className="hkDescription">Hold shift + option while selecting the object</p>
+
+              </div>
+
+          </div>
+
+          <div className="col-md-4">
+
+              <div>
+
+                  <h4 className="hkTitle">Pointer, a.k.a. Move Tool</h4>
+
+                  <p className="hkDescription">v</p>
+
+              </div>
+
+          </div>
+
+          <div className="col-md-4">
+
+              <div>
+
+                  <h4 className="hkTitle">Select Tool</h4>
+
+                  <p className="hkDescription">m</p>
+
+              </div>
+
+          </div>
+
+          <div className="col-md-4">
+
+              <div>
+
+                  <h4 className="hkTitle">Eyedropper</h4>
+
+                  <p className="hkDescription">i</p>
+
+              </div>
+
+          </div>
+
+          <div className="col-md-4">
+
+              <div>
+
+                  <h4 className="hkTitle">Magic Wand</h4>
+
+                  <p className="hkDescription">w</p>
+
+              </div>
+
+          </div>
+
+
         </div>
-      </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      </div>
+    </Layout>
   )
 }
